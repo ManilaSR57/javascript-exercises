0d913aac1cc3de48adb97c1328a8ceaf7cddfb82
ManilaSR57 <kiriilcebekov@gmail.com>
2026-03-31 22:17:48 +0900

    Solved 04_removeFromArray. Passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    if (Array.isArray(args)) {
+        return arr.filter(x => !args.includes(x));
+    }
+    return arr.filter(x => x !== args);
 };
 
 // Do not edit below this line
